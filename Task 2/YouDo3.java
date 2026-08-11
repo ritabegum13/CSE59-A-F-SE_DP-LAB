5eeeb414b93ee8be2f7e1dfd30c597cf844f3f95
ritabegum13 <duhitarita13@gmail.com>
2026-08-11 23:44:39 +0600

    Update YouDo3.java

--- Task 2/YouDo3.java
+++ Task 2/YouDo3.java
@@ -1,1 +1,19 @@
+public class Countdown {
 
+    public void start() {
+        for (int i = 5; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+        System.out.println("Go!");
+    }
+}
+public class Main {
+
+    public static void main(String[] args) {
+
+        Countdown countdown = new Countdown();
+
+        countdown.start();
+    }
+}
