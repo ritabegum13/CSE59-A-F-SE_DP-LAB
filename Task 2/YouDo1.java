3c9333744c996766b283a453c08db92b36dbc830
ritabegum13 <duhitarita13@gmail.com>
2026-08-11 23:36:04 +0600

    Update YouDo1.java

--- Task 2/YouDo1.java
+++ Task 2/YouDo1.java
@@ -1,1 +1,14 @@
+class ContestStats {
+    int[] scores = {85, 90, 95};
 
+    void printFirstScore() {
+        System.out.println(scores[0]);
+    }
+}
+
+public class YouDo1 {
+    public static void main(String[] args) {
+        ContestStats obj = new ContestStats();
+        obj.printFirstScore();
+    }
+}
