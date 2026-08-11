c56c2094e3321fb75152257441b5ff43e322eb6e
ritabegum13 <duhitarita13@gmail.com>
2026-08-11 23:39:04 +0600

    Update YouDo2.java

--- Task 2/YouDo2.java
+++ Task 2/YouDo2.java
@@ -1,1 +1,19 @@
+public class NumberChecker {
 
+    public void check(int num) {
+        if (num % 2 == 0 && num > 10) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
+public class Main {
+
+    public static void main(String[] args) {
+
+        NumberChecker obj = new NumberChecker();
+
+        obj.check(14);
+    }
+}
