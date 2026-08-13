0584f86cad149af5b207a5ca4159b89a2b0b9d77
ritabegum13 <duhitarita13@gmail.com>
2026-08-13 15:09:59 +0600

    Update YouDo5.java

--- Task 2/YouDo5.java
+++ Task 2/YouDo5.java
@@ -1,1 +1,21 @@
+public class YouDo5 {
 
+    public static void main(String[] args) {
+
+        Printer printer = new Printer();
+
+        printer.printData("Hello, World!");
+        printer.printData(100);
+    }
+}
+
+class Printer {
+
+    public void printData(String text) {
+        System.out.println(text);
+    }
+
+    public void printData(int number) {
+        System.out.println(number);
+    }
+}
