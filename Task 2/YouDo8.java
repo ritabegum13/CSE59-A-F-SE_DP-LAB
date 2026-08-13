fe912fd8045be0e3d1bcfbd7900c8b1a84fcc901
ritabegum13 <duhitarita13@gmail.com>
2026-08-13 15:13:30 +0600

    Update YouDo8.java

--- Task 2/YouDo8.java
+++ Task 2/YouDo8.java
@@ -1,1 +1,39 @@
+public class YouDo8 {
 
+    public static void main(String[] args) {
+
+        Item item1 = new Item("Mouse", 20.0);
+        Item item2 = new Item("Keyboard", 45.0);
+
+        Order order = new Order(item1, item2);
+
+        order.printBill();
+    }
+}
+
+class Item {
+
+    String name;
+    double price;
+
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+}
+
+class Order {
+
+    Item item1;
+    Item item2;
+
+    public Order(Item item1, Item item2) {
+        this.item1 = item1;
+        this.item2 = item2;
+    }
+
+    public void printBill() {
+        double total = item1.price + item2.price;
+        System.out.println("Total cost: " + total);
+    }
+}
