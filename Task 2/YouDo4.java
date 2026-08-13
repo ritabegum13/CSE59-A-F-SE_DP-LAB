a4dfc68c502eca280cf7a8ac8492a00248f6d66e
ritabegum13 <duhitarita13@gmail.com>
2026-08-13 15:08:42 +0600

    Update YouDo4.java

--- Task 2/YouDo4.java
+++ Task 2/YouDo4.java
@@ -1,1 +1,23 @@
 
+public class YouDo4 {
+
+    public static void main(String[] args) {
+
+        Sensor sensor = new Sensor();
+
+        sensor.charge();
+
+        System.out.println("Battery Level: " + sensor.batteryLevel);
+    }
+}
+
+class Sensor {
+
+    String name;
+    int batteryLevel;
+
+    public void charge() {
+        batteryLevel = 100;
+        System.out.println("Sensor fully charged");
+    }
+}
