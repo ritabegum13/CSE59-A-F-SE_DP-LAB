df742dd194c52dc02dd11d336f80651779eca440
ritabegum13 <duhitarita13@gmail.com>
2026-08-13 15:11:15 +0600

    Update YouDo6.java

--- Task 2/YouDo6.java
+++ Task 2/YouDo6.java
@@ -1,1 +1,24 @@
+public class YouDo6 {
 
+    public static void main(String[] args) {
+
+        GameApplet game = new GameApplet();
+
+        game.launch();
+        game.play();
+    }
+}
+
+class Applet {
+
+    public void launch() {
+        System.out.println("Applet launched");
+    }
+}
+
+class GameApplet extends Applet {
+
+    public void play() {
+        System.out.println("Playing game");
+    }
+}
